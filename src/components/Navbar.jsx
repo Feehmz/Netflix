03da9c71380d2af18c22322450d98b5e4a4acc0e
Federico Peducci <federico.peducci@edu-its.it>
2025-11-14 11:40:08 +0100

    fatto il merge del branch homepage con il main

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+import "../styles/global.css";
+
+export default function Navbar() {
+  return (
+    
+    <nav className="navbar">
+      <Link to="/" className="logo">NetFlexx</Link>
+      <div className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/movie/1">Film esempio</Link>
+      </div>
+    </nav>
+  );
+}
