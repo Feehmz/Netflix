5490f821179bcf391623defedfcec0b41dd53d31
Federico Peducci <federico.peducci@edu-its.it>
2025-11-17 12:02:01 +0100

    fix: layout e app

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,21 @@ import Homepage from "./pages/HomePage";
 import ListaFilm from "./pages/ListaFilm";
 import MoviePage from "./pages/MoviePage";
 import ErrorPage from "./pages/ErrorPage";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer"
+import Layout from "./Layout/Layout";
 
 import "./styles/global.css";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/lista" element={<ListaFilm />} />
-        <Route path="/movie/:id" element={<MoviePage />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
-      <Footer/>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/lista" element={<ListaFilm />} />
+          <Route path="/movie/:id" element={<MoviePage />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 }
