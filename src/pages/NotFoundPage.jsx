cfff0a9019501266ed60472708c884adf2b2a197
feeh <lavoltaantica87@gmail.com>
2025-11-24 19:52:36 +0100

    style: modificato il css della NotFoundPage

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,12 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"; 
 import "../styles/NotFoundPage.css";
 
 export default function NotFoundPage() {
   return (
-    <div className="notfound">
-      <h1>404</h1>
-      <p>La pagina che stai cercando non esiste.</p>
-      <Link to="/">Torna alla Home</Link>
+    <div className="notfound-page">
+      <div className="notfound-content">
+        <h1 className="notfound-code">404</h1>
+        <p className="notfound-message">
+          Ops! La pagina che stai cercando non esiste.
+        </p>
+        <Link to="/" className="notfound-btn">Torna alla Home</Link>
+      </div>
     </div>
   );
 }
